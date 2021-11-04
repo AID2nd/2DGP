b168b20b84371bb76ae5d80b3933e1fe83560de2
LeeJunHyeok <chocojh1223@naver.com>
2021-11-04 19:29:13 +0900

    DRILL #11
    https://youtu.be/gSk-0iaNxR4

--- Labs/Lecture11_Character_Controller/boy.py
+++ Labs/Lecture11_Character_Controller/boy.py
@@ -1,23 +1,139 @@
 from pico2d import *
 
 # Boy Event
-# fill here
+RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP, SLEEP_TIMER, DASH_UP, DASH_DOWN = range(7)     # 0부터 3까지의 정수값이 차례로 할당됨. (튜플생성)
 
-key_event_table = {
-    (SDL_KEYDOWN, SDLK_RIGHT): RIGHT_DOWN,
+key_event_table = {         # 딕셔너리를 이용한 키매핑
+    (SDL_KEYDOWN, SDLK_RIGHT): RIGHT_DOWN,      # Key(정수,정수) : Value(정수).  Key는 튜플
     (SDL_KEYDOWN, SDLK_LEFT): LEFT_DOWN,
     (SDL_KEYUP, SDLK_RIGHT): RIGHT_UP,
-    (SDL_KEYUP, SDLK_LEFT): LEFT_UP
+    (SDL_KEYUP, SDLK_LEFT): LEFT_UP,
+    (SDL_KEYDOWN, SDLK_LSHIFT): DASH_DOWN,
+    (SDL_KEYUP, SDLK_LSHIFT): DASH_UP
 }
-
 
 
 # Boy States
 
-# fill here
+class IdleState:
+    def enter(boy, event):
+        if event == RIGHT_DOWN:
+            boy.velocity += 1
+        elif event == LEFT_DOWN:
+            boy.velocity -= 1
+        elif event == RIGHT_UP:
+            boy.velocity -= 1
+        elif event == LEFT_UP:
+            boy.velocity += 1
+        boy.timer = 1000
+
+    def exit(boy, event):
+        pass
+
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+        boy.timer -= 1
+        if boy.timer == 0:
+            boy.add_event(SLEEP_TIMER)
+
+    def draw(boy):
+        if boy.dir == 1:
+            boy.image.clip_draw(boy.frame * 100, 300, 100, 100, boy.x, boy.y)
+        else:
+            boy.image.clip_draw(boy.frame * 100, 200, 100, 100, boy.x, boy.y)
+
+class RunState:
+    def enter(boy, event):
+        if event == RIGHT_DOWN:
+            boy.velocity += 1
+            if event == DASH_DOWN:
+                boy.velocity += 5
+            elif event == DASH_UP:
+                boy.velocity -= 5
+        elif event == LEFT_DOWN:
+            boy.velocity -= 1
+            if event == DASH_DOWN:
+                boy.velocity += 5
+            elif event == DASH_UP:
+                boy.velocity -= 5
+        elif event == RIGHT_UP:
+            boy.velocity -= 1
+        elif event == LEFT_UP:
+            boy.velocity += 1
+        boy.dir = boy.velocity
+
+    def exit(boy, event):
+        pass
+
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+        boy.timer -= 1
+        boy.x += boy.velocity
+        boy.x = clamp(25, boy.x, 800 - 25)
+
+    def draw(boy):
+        if boy.velocity == 1:
+            boy.image.clip_draw(boy.frame * 100, 100, 100, 100, boy.x, boy.y)
+        else:
+            boy.image.clip_draw(boy.frame * 100, 0, 100, 100, boy.x, boy.y)
+
+class SleepState:
+    def enter(boy, event):
+        boy.frame = 0
+
+    def exit(boy, event):
+        pass
+
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+
+    def draw(boy):
+        if boy.dir == 1:
+            boy.image.clip_composite_draw(boy.frame * 100, 300, 100, 100, 3.141592 / 2, '', boy.x - 25, boy.y - 25, 100, 100)
+
+        else:
+            boy.image.clip_composite_draw(boy.frame * 100, 200, 100, 100, -3.141592 / 2, '', boy.x + 25, boy.y - 25, 100, 100)
+
+class DashState:
+    def enter(boy, event):
+        if event == DASH_DOWN:
+            print("Shift 누름")
+            boy.velocity -= 3
+        if event == DASH_UP:
+            print("Shift 뗌")
+            boy.velocity -= 3
+        boy.dir = boy.velocity
+
+    def exit(boy, event):
+        pass
+
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+        boy.timer -= 1
+        boy.x += boy.velocity
+        boy.x = clamp(25, boy.x, 800 - 25)
+
+    def draw(boy):
+        if boy.velocity == 1:
+            boy.image.clip_draw(boy.frame * 100, 100, 100, 100, boy.x, boy.y)
+        else:
+            boy.image.clip_draw(boy.frame * 100, 0, 100, 100, boy.x, boy.y)
+
+
+
 
 next_state_table = {
-# fill here
+    IdleState: {RIGHT_UP: RunState, LEFT_UP: RunState,
+                RIGHT_DOWN: RunState, LEFT_DOWN: RunState,
+                SLEEP_TIMER: SleepState},
+    RunState: {RIGHT_UP: IdleState, LEFT_UP: IdleState,
+               LEFT_DOWN: IdleState, RIGHT_DOWN: IdleState,
+               DASH_UP: DashState, DASH_DOWN: RunState},
+    SleepState: {LEFT_DOWN: RunState, RIGHT_DOWN: RunState,
+                 LEFT_UP: RunState, RIGHT_UP: RunState},
+    DashState: {RIGHT_UP: IdleState, LEFT_UP: IdleState,
+                RIGHT_DOWN: DashState, LEFT_DOWN: DashState,
+                DASH_UP: RunState, DASH_DOWN: IdleState}
 }
 
 
@@ -33,8 +149,11 @@ class Boy:
         self.image = load_image('animation_sheet.png')
         self.dir = 1
         self.velocity = 0
-        # fill here
-        pass
+        self.frame = 0
+        self.timer = 0
+        self.event_que = []
+        self.cur_state = IdleState
+        self.cur_state.enter(self, None)
 
 
     def change_state(self,  state):
@@ -43,21 +162,24 @@ class Boy:
 
 
     def add_event(self, event):
-        # fill here
-        pass
+        self.event_que.insert(0, event)
 
 
     def update(self):
-        # fill here
-        pass
+        self.cur_state.do(self)
+        if len(self.event_que) > 0:
+            event = self.event_que.pop()
+            self.cur_state.exit(self, event)
+            self.cur_state = next_state_table[self.cur_state][event]
+            self.cur_state.enter(self, event)
 
 
     def draw(self):
-        # fill here
-        pass
+        self.cur_state.draw(self)
+        debug_print('Velocity :' + str(self.velocity) + '  Dir:' + str(self.dir))
 
 
     def handle_event(self, event):
-        # fill here
-        pass
-
+        if (event.type, event.key) in key_event_table:
+            key_event = key_event_table[(event.type, event.key)]
+            self.add_event(key_event)
